f3e5928e4fc168e59914d4c01abef1e24bad301b
Vanessacor <vanessapcbarros@gmail.com>
2020-09-15 12:23:50 +0200

    feat: create get/:id and post/ routes

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -12,4 +12,25 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/:id', async (req, res, next) => {
+  try {
+    const data = await Event.findById(req.params.id).exec();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, date, location } = req.body;
+    const event = new Event({ name, date, location });
+    const data = await event.save();
+    const result = res.status(201).json(data);
+    return result;
+  } catch (err) {
+    next(err);
+  }
+});
+
 export default router;
